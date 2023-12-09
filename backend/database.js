9cb01af26845e560fecfc8124d85d0df2c0c28ef
pranjal-yadav05 <126938164+pranjal-yadav05@users.noreply.github.com>
2023-12-09 10:41:10 +0530

    model tuning added

--- backend/database.js
+++ backend/database.js
@@ -726,6 +726,45 @@ app.post("/process-input", async (req, res) => {
   }
   )();
     
+// const MODEL_NAME = "models/text-bison-001";
+// const API_KEY = "PALM_API_KEY";
+
+// const client = new TextServiceClient({
+//   authClient: new GoogleAuth().fromAPIKey(API_KEY),
+// });
+
+// const input = '';
+// const promptString = `input: what is a transformer?
+// output: A transformer is a device that transfers electric energy from one alternating-current circuit to one or more other circuits, either increasing (stepping up) or reducing (stepping down) the voltage.
+// input: ${userInput}
+// output:`;
+// const stopSequences = [];
+
+// client.generateText({
+//   // required, which model to use to generate the result
+//   model: MODEL_NAME,
+//   // optional, 0.0 always uses the highest-probability result
+//   temperature: 0.7,
+//   // optional, how many candidate results to generate
+//   candidateCount: 1,
+//   // optional, number of most probable tokens to consider for generation
+//   topK: 40,
+//   // optional, for nucleus sampling decoding strategy
+//   topP: 0.95,
+//   // optional, maximum number of output tokens to generate
+//   maxOutputTokens: 1024,
+//   // optional, sequences at which to stop model generation
+//   stopSequences: stopSequences,
+//   // optional, safety settings
+//   safetySettings: [{"category":"HARM_CATEGORY_DEROGATORY","threshold":"BLOCK_LOW_AND_ABOVE"},{"category":"HARM_CATEGORY_TOXICITY","threshold":"BLOCK_LOW_AND_ABOVE"},{"category":"HARM_CATEGORY_VIOLENCE","threshold":"BLOCK_MEDIUM_AND_ABOVE"},{"category":"HARM_CATEGORY_SEXUAL","threshold":"BLOCK_MEDIUM_AND_ABOVE"},{"category":"HARM_CATEGORY_MEDICAL","threshold":"BLOCK_MEDIUM_AND_ABOVE"},{"category":"HARM_CATEGORY_DANGEROUS","threshold":"BLOCK_MEDIUM_AND_ABOVE"}],
+//   prompt: {
+//     text: promptString,
+//   },
+// }).then(result => {
+//   console.log(result[0].candidates[0].output);
+//   res.send(result[0].candidates[0].output);
+// });
+
 });
 var chunks = []
 
